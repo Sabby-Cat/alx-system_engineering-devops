ec001879f6327b22e4506ee459465da0162d2124
Sabrina Beukes <sabrinabeukes@gmail.com>
2024-02-23 13:09:25 -0800

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 #install flask 2.1.0
-class flask_installation {
-  package { 'python3-pip':
-    ensure => installed,
-  }
-
-  exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
-    path    => ['/usr/bin'],
-    require => Package['python3-pip'],
-  }
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
