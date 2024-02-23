06310de95f5f203bdfecacf97edbf0f8423d55cb
Sabrina Beukes <sabrinabeukes@gmail.com>
2024-02-23 13:31:28 -0800

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 #install flask 2.1.0
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Werkzeug==2.1.1 Flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
